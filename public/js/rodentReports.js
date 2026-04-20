c6d3932080e216ce61f8654fcb62f7c1f518b04b
Peter Driscoll <peteredriscoll@gmail.com>
2026-04-21 06:26:37 +0700

    added clear filter button and set up initial date filtering for RodentReports

--- public/js/rodentReports.js
+++ public/js/rodentReports.js
@@ -30,6 +30,7 @@ document.querySelector(".leftPane").addEventListener("click", async (e) => {
 });
 
 
+//FILTERING FUNCTIONS FOR RODENT REPORTS
 //Filters out non-verified reports based on filter checkbox
 document.getElementById("verifiedStatus").addEventListener('change', function() {
   if (this.checked) {
@@ -53,3 +54,84 @@ document.getElementById("verifiedStatus").addEventListener('change', function() 
 
   }
 });
+
+
+//returns current date as str "YYYY-MM-DD" format to be used for join date
+const getDateNow=()=>{
+    const date = new Date().toLocaleDateString('en-US', {
+        month: '2-digit',
+        day: '2-digit',
+        year: 'numeric'
+    });
+
+    return date
+} 
+
+
+//Auto sets the date filters to be today and last 30 days
+const today = new Date().toISOString().split('T')[0];
+let monthAgo = new Date();
+monthAgo.setDate(monthAgo.getDate() - 30);
+monthAgo = monthAgo.toISOString().split('T')[0];
+let startDate = document.getElementById("startDate");
+startDate.value = monthAgo
+let endDate = document.getElementById("endDate");
+endDate.value = today
+
+//Compares a date against a filter date. Returns true if first date1 came after date2
+const compareDate =(date1, date2)=>{
+    //both inputs are strings. convert back to date and then compare
+    date1 = new Date(date1)
+    date2 = new Date(date2)
+    return date1 > date2
+}
+
+
+//add event listeners to both date selectors. on change update the visbility of 
+document.getElementById('startDate').addEventListener('change', function() {
+    //get the date value of each
+    //filter the cards that are not in that date range (if not already filtered)
+    let cards = document.querySelectorAll(".ratCard");
+    for(card of cards){
+        //check if that data-id should be hidden since its unverified
+        let cardDate = card.attributes['data-timestamp'].value
+
+        //if that date is < start date then hide
+        if(!compareDate(cardDate, startDate)){
+            card.style.display = 'none';
+        }
+    }
+})
+
+//add event listeners to both date selectors. on change update the visbility of 
+document.getElementById('endDate').addEventListener('change', function() {
+    //get the date value of each
+    //filter the cards that are not in that date range (if not already filtered)
+    let cards = document.querySelectorAll(".ratCard");
+    for(card of cards){
+        //check if that data-id should be hidden since its unverified
+        let cardDate = card.attributes['data-timestamp'].value
+
+        //if that date is > end date then hide
+        if(compareDate(cardDate, endDate)){
+            card.style.display = 'none';
+        }
+    }
+})
+
+//A clear filters button RESETS ALL FILTERS
+document.getElementById('clearFilters').addEventListener("click", function(){
+    
+    //set all filters back to default
+    let verifiedButton = document.getElementById("verifiedStatus")
+    verifiedButton.checked = false
+
+    startDate.value = monthAgo
+    endDate.value = today
+   
+    //Set all cards back to show
+    let cards = document.querySelectorAll(".ratCard");
+    for(card of cards){
+        card.style.display = '';
+    }
+})
