8d0a5131c0f3eff6a3b26581b13ddb939792fa40
Olivia Longmuir <60204728+olivialongmuir@users.noreply.github.com>
2026-04-20 19:32:58 -0400

    Feature/styling map (#17)
    * formatting the heatmap
    
    * formatting, adding data to hotspot feed
    
    * styling

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -31,9 +31,18 @@ router.route('/heatmap').get(async (req, res) => {
 
         const restaurantMapData = JSON.stringify(restaurantData);
 
+        // TODO - hone in on hotspot feed
+        // potentially grab restaurants with top comments
+        // or grab rodent data with top comments
+        const hotspotFeed = restaurantList.slice(0, 9).map(r => ({
+            name: r.name,
+            status: r.status ?? "Status Unavailable"
+        }));
+
         res.render("heatmap", {
             title: 'Heatmap',
-            restaurantMapData: restaurantMapData
+            restaurantMapData: restaurantMapData,
+            hotspotFeed: hotspotFeed
             // TODO - pass in rodent data
         });
 
