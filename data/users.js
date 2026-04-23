809bcc763d4af0866cc2f82c9687e48126a97e6e
Ali Taseer <ali.taseer97@gmail.com>
2026-04-22 20:51:26 -0400

    User registration, login,
    Add user registration and fix password validation field name
    Add createUser in data/users.js with bcrypt hashing and username/email checks
    Add GET/POST /register routes
    Add register.handlebars view and sing up link
    Add checkUserType helper
    Make profile account type a readonly input

--- data/users.js
+++ data/users.js
@@ -1,29 +1,32 @@
 import { users } from "../config/mongoCollections.js";
-import { checkId } from "../helpers.js";
+import {
+    checkId,
+    checkString,
+    checkUsername,
+    checkEmail,
+    checkPassword,
+    checkUserType
+} from "../helpers.js";
 import { ObjectId } from "mongodb";
+import bcrypt from "bcryptjs";
 
-/**
- * Gets all users from database as a list of objects
- * @returns usersList
- */
-export const getAllUsers = async() => {
-    //Get users collection from database
+const SALT_ROUNDS = 10;
+
+export const getAllUsers = async () => {
     const usersCollection = await users();
     let userList = await usersCollection.find({}).toArray();
-
-    //Check if database returned anything
     if (!userList) throw "Error {getAllUsers}: No user accounts in database";
 
-    //Convert all object ids to string ids
     userList = userList.map(user => {
-        user.__id = user.__id.toString();
-        return user
-    })
+        user._id = user._id.toString();
+        delete user.hashPassword;
+        return user;
+    });
 
-    return userList
+    return userList;
 };
 
-export const getUserById = async(id) => {
+export const getUserById = async (id) => {
     const parsedId = checkId(id);
     const usersCollection = await users();
     const user = await usersCollection.findOne({ _id: new ObjectId(parsedId) });
@@ -32,18 +35,60 @@ export const getUserById = async(id) => {
     return user;
 };
 
-export const createUser = async() => {
+export const createUser = async ({
+    firstName,
+    lastName,
+    username,
+    emailAddress,
+    password,
+    type
+}) => {
+    const validatedFirstName = checkString(firstName, "firstName");
+    const validatedLastName = checkString(lastName, "lastName");
+    const validatedUsername = checkUsername(username);
+    const validatedEmail = checkEmail(emailAddress).toLowerCase();
+    const validatedPassword = checkPassword(password);
+    const validatedType = checkUserType(type);
+
+    const usersCollection = await users();
+
+    const existingByUsername = await usersCollection.findOne({ username: validatedUsername });
+    if (existingByUsername) throw "Error: Username is already taken";
+
+    const existingByEmail = await usersCollection.findOne({ emailAddress: validatedEmail });
+    if (existingByEmail) throw "Error: An account with that email already exists";
+
+    const hashPassword = await bcrypt.hash(validatedPassword, SALT_ROUNDS);
+
+    const newUser = {
+        type: validatedType,
+        firstName: validatedFirstName,
+        lastName: validatedLastName,
+        username: validatedUsername,
+        emailAddress: validatedEmail,
+        hashPassword,
+        timestamp: new Date().toISOString(),
+        comments: []
+    };
+
+    const insertInfo = await usersCollection.insertOne(newUser);
+    if (!insertInfo.acknowledged || !insertInfo.insertedId) {
+        throw "Error {createUser}: Could not create user";
+    }
+
+    const inserted = { ...newUser, _id: insertInfo.insertedId.toString() };
+    delete inserted.hashPassword;
+    return inserted;
+};
+
+export const updateUser = async () => {
 
 };
 
-export const updateUser = async() => {
+export const deleteUser = async () => {
 
 };
 
-export const deleteUser = async() => {
-
-};
-
-export const getUserComments = async() => {
+export const getUserComments = async () => {
 
 };
