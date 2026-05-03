ebbd6372b94d4b7f3975f1168a5685fa99f4da19
Peter Driscoll <peteredriscoll@gmail.com>
2026-05-03 18:17:34 +0700

    set up initial handlebars partial for rodent reports. Still needs data passed in

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -127,35 +127,18 @@ router.route('/heatmap').get(async (req, res) => {
     }
 });
 
-//Generic Route which is called from the nav bar route
+//Generic Route which is called from the nav bar route. Will send user to first available rodent report page.
 router.route('/ratreports').get(async (req, res) => {
     try {
 
-        //TODO -  error handling and invalid data checking
-
+        //TODO - handle case when no reports in DB
         //get all reports
         let reports = await getAllReports();
-
-        //set the minimaps location to be first report if available
+        //get a single report
         let firstReport = reports[0]
-
-        const firstLocation = {
-            name: firstReport.name,
-            lat: Number(firstReport.latitude),
-            lng: Number(firstReport.longitude)
-        }
-
-        //put object in arr since its iterated when the maps built
-        const restaurantData = [firstLocation];
-
-        const restaurantMapData = JSON.stringify(restaurantData);
-
-        res.render("rodentReports", {
-            title: 'Rodent Reports',
-            reports:reports,
-            restaurantMapData: restaurantMapData,
-            firstReport: firstReport //passing in first report which will be the default starting data shown
-        });
+        let id = firstReport._id
+        //redirect user to that reports page
+        res.redirect(`/ratreports/${id}`);
     } catch (error) {
         console.error(error);
         res.status(500).send("Error loading Rodent Reports");
@@ -189,11 +172,17 @@ router.route('/ratreports/:id').get(async (req, res) => {
 
         const restaurantMapData = JSON.stringify(restaurantData);
 
+
+        //Load in the comments from that report
+        //const comments = await getRestaurantComments(id);
+
         res.render("rodentReports", {
             title: 'Rodent Reports',
             reports:reports,
             restaurantMapData: restaurantMapData,
-            firstReport: targetReport //passing in target report
+            firstReport: targetReport, //passing in target report
+            comments:null,
+            currentUserId: req.session.userId
         });
     } catch (error) {
         console.error(error);
