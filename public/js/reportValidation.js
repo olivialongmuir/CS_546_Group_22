79e9bcd37724288f1ff9667222029548ff555ada
Peter Driscoll <peteredriscoll@gmail.com>
2026-05-07 21:39:21 +0700

    Client side validation for rodent reporting from map page

---
+++ public/js/reportValidation.js
@@ -0,0 +1,35 @@
+import * as helpers from "./helpers.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    //client side validation of the rodent report form
+    const rodentForm = document.getElementById("rodentForm");
+
+    rodentForm.addEventListener("submit", (e)=>{
+
+
+        const description = document.getElementById("description").value.trim();
+        const latitude = document.getElementById("latitude").value.trim();
+        const longitude = document.getElementById("longitude").value.trim();
+
+
+        const zipcode = document.getElementById("zipcode").value.trim();
+
+        let error;
+
+        //Validation of each
+        try{
+            let validatedDescription = helpers.checkDescription(description);
+            let validatedLatitude = helpers.checkLatitude(latitude);
+            let validatedLongitude = helpers.checkLongitude(longitude);
+            let validatedZipcode = helpers.checkZipcode(zipcode);
+        }catch(err){
+            //stop the form
+            e.preventDefault();
+            //show in the form error
+            const errorBox = document.getElementById("formErrors");
+            errorBox.innerHTML = `<p>${err}</p>`
+            errorBox.hidden = false;
+        }
+    });
+});
